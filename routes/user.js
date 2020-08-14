2b87868d832734c4cddd9586058ff94a31996d3e
Sebastian Hernandez <sebhernandezram@gmail.com>
2020-08-14 01:36:43 -0500

    Get all users route

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { getAll, getCurrent, create, login } = require('../controllers/user');
 
-router.get('/', getAll);
-router.get('/current', getCurrent);
+router.get('/', getCurrent);
+router.get('/all', getAll);
 router.post('/signup', create);
 router.post('/login', login);
 
